45238fa73aab83122308a15e76ddb5729ca6585b
tainahcarvalho21 <tainah_carvalho@hotmail.com>
2023-11-09 21:11:24 -0300

    improvements

--- app.js
+++ app.js
@@ -3,23 +3,20 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = 3000; // Defina a porta que desejar
+const port = 3000; 
 
-// Configuração do servidor e rota para a raspagem
+
 app.get('/api/scrape', async (req, res) => {
   const keyword = req.query.k;
 
   try {
     const driver = await new Builder().forBrowser('chrome').build();
     await driver.get(`https://www.amazon.com.br/s?k=${keyword}`);
-
-    // Aguarde até que a página esteja totalmente carregada
+    
     await driver.wait(until.urlContains(`https://www.amazon.com.br/s?k=${keyword}`), 10000);
 
-    // Obtenha o HTML da página após o carregamento completo
     const html = await driver.getPageSource();
 
-    // Use Cheerio para analisar o HTML
     const $ = cheerio.load(html);
 
     const products = [];
@@ -37,14 +34,14 @@ app.get('/api/scrape', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while scraping Amazon.' });
   } finally {
-    // Feche o navegador após a conclusão
+    
     if (driver) {
       await driver.quit();
     }
   }
 });
 
-// Iniciar o servidor
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
